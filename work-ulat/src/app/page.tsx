b88d2d13ada8a52e4f014a59f67fe8f3b60a0751
MarcusOcampo <ammo.merks@gmail.com>
2025-03-26 20:37:38 +0800

    initialize nextjs

---
+++ work-ulat/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen">
+      <p className="text-black text-3xl">Dito yung dashboard</p>
+    </div>
+  );
+}
